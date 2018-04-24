c4148ace3e78c00f5b8f28cc165957fd50da4aac
huangfuhuijie <32241657+huangfuhuijie@users.noreply.github.com>
2018-04-25 00:05:10 +0800

    0.01

--- pages/search/search.js
+++ pages/search/search.js
@@ -5,14 +5,18 @@ Page({
    */
   data: {
     inputValue:"",
-    basic_explain:[""],
-    phonetic:"",
-    web_explain:[""],
-    success:0,
+    basic_explain:[""],//基本释义
+    phonetic:"",//音标
+    web_explain:[""],//网络释义
+    success_1:0,//有道成功标志
+    success_2:0,//维基百科成功标志
+    extract:"",//专业释义
+    img:"",//图片地址
   },
 
   finish_input: function (e) {
     var that = this;
+    /*****************************分割线，以下是有道翻译的request*************/
     var quary = this.data.inputValue;
     var appKey = '5918635af2f93979';
     var key = 'kfdoFIC8mEoNL0lMZkgsfElCify3yXhM';
@@ -21,7 +25,7 @@ Page({
     var md5 = require('../../utils/md5.js');
     var realsign = md5(sign);
     wx.request({
-      url: 'http://openapi.youdao.com/api',
+      url: 'https://openapi.youdao.com/api',
       type:'post',
       dataType:'jsonp',
       data:{
@@ -39,20 +43,57 @@ Page({
         if(my.errorCode==0)
         {
           that.setData({
-            success:1,
+            success_1:1,
             basic_explain: my.basic.explains,
-            phonetic: my.basic.phonetic,
             web_explain: my.web,
           })
         }
+        if(my.basic.phonetic!= null)
+        {
+          that.setData({
+            phonetic: my.basic.phonetic,
+          })
+        }
+        if (that.data.success_1 == 1 && that.data.success_2 == 1) {//停止转圈
+          wx.hideNavigationBarLoading();
+        }
+      },
+    })
+
+    /*****************************分割线，以下是维基百科的request*************/
+    var url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=1&prop=description|pageimages&pithumbsize=300&origin=*&gsrsearch='
+    wx.request({
+      url: url+this.data.inputValue,
+      success: function (data) {
+        if (that.data.success_1 == 1 && that.data.success_2 == 1) {
+          wx.hideNavigationBarLoading();
+        }
+        console.log(data);
+        var pageid;
+        for(var page_id in data.data.query.pages){
+          pageid=page_id;
+        }
+        var page = data.data.query.pages[pageid];
+        var extract = page.description;
+        var img = page.thumbnail.source;
+        that.setData({
+          success_2:1,
+          extract:extract,
+          img:img,
+        });
+        if (that.data.success_1 == 1 && that.data.success_2 == 1) {
+          wx.hideNavigationBarLoading();
+        }
       }
     })
+    wx.showNavigationBarLoading()
   },
 
 
   bindinput:function(e){
     this.setData({
-      success:0,
+      success_1:0,
+      success_2:0,
       inputValue: e.detail.value
     })
   },
